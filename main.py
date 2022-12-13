c97f679b8c51456ddef1729ccebc1504a2c0ebe0
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2022-12-13 19:12:36 +0000

    Fixed BFS implementation to handle cycles

--- main.py
+++ main.py
@@ -29,78 +29,38 @@ input: [["B","A"],["C","A"],["D","C"],["E","D"],["E","B"]]
 """
 
 from collections import defaultdict
-"""
-def topology_sort(node, adj_list, visited_list, result):
-  visited_list[node] = True
-
-  # recurse on all unvisited neighbors 
-  for n in adj_list[node]:
-    print("AT node: ", node)
-    print("Checking neighbor: ", n)
-    if visited_list[n] == False:
-      topology_sort(n, adj_list, visited_list, result)
-  # add current node to stack
-  result.append(node)
-  print("Done checking node, STACK:", result)
-  return result
-  
-  
 
 def find_compilation_order(dependencies):
-  # Write your code here
-
-  adj_list, visited_list = defaultdict(list), defaultdict(bool)
+  # pre processing
+  # build adj list, visited list, in degree
+  adj_list, in_degree, visited = defaultdict(list), defaultdict(int), defaultdict(bool)
   for dep in dependencies:
-    adj_list[dep[1]].append(dep[0])
-    if dep[0] not in adj_list:
-      adj_list[dep[0]] = []
-    visited_list[dep[0]] = False
-    visited_list[dep[1]] = False
+    start, end = dep[1], dep[0]
+    adj_list[start].append(end)
+    in_degree[end] +=1
+    if start not in in_degree:
+      in_degree[start] = 0
+  print(adj_list, in_degree, visited)
+  # traverse graph, topo sort
   result = []
-  for node in adj_list:
-    if visited_list[node] == False:
-      topology_sort(node, adj_list, visited_list, result)
-  print(adj_list)
-  return result[::-1]
+  q = []
+  # init q 
+  for node in in_degree:
+    if in_degree[node] == 0:
+      q.append(node)
+  while q:
+    elem = q.pop(0)
+    visited[elem] = True
+    result.append(elem)
+    # parse neighbors
+    for n in adj_list[elem]:
+      in_degree[n] -=1
+      if visited[n] == False and in_degree[n]==0:
+        q.append(n) # parse neighbors with 0 dependencies
+  return result
 
-#print(find_compilation_order([["B","A"],["C","A"],["D","C"],["E","D"],["E","B"]]))
-print(find_compilation_order([["B","A"],["A","B"]]))
+print(find_compilation_order([["B","A"],["C","A"],["D","C"],["E","D"],["E","B"]]))
 
-"""
-# from collections import defaultdict
- 
-class Graph:
-    def __init__(self, vertices):
-        self.graph = defaultdict(list)
-        self.V = vertices
-    
-    def add_edge(self, u, v):
-        self.graph[u-1].append(v-1)
-    def helper(self,u,visited, res):
-        print("visiting: ", u)
-        for i in self.graph[u]:
-            print("Neighbors of", u)
-            print("looking at neigh:", i)
-            if visited[i] == False:
-                self.helper(i, visited, res)
-        visited[u] = True
-        res.insert(0, u+1)
-    def topological_sort(self):
-        visited = [False]*self.V
-        print(visited)
-        res =[]
-        for i in reversed(range(self.V)):
-            if visited[i] == False:
-                self.helper(i, visited, res)
-                
-        return res
-## Driver code
-## Driver code  
-A = 2
-B = [[1, 2], [2, 1]]
-graph = Graph(A)
-for u, v in B:
-    graph.add_edge(u, v)
-        
-res = graph.topological_sort()
-print(res) # [5, 6, 1, 3, 4, 2]
+print(find_compilation_order([["E","A"],["B","A"],["C","E"],["D","E"],["B","D"]]))
+
+print(find_compilation_order([["A","B"],["B","A"]]))
